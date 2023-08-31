9b42c728be6d704766e05c7f6d2b05d760cfb49f
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-09-01 01:43:48 +0200

    Changelogs: - Added work-in-progress help.php and settings.php pages. - Minor changes.
    TODO (When possible):
    - Add edit cover option to gallery.php.
    - Improve design for edit options of gallery.php.
    - Add edit comment option to comments to share.php.
    - Redesign comments with something a bit more modern.
    - Edit gallery cover with an image feature.
    - Edit gallery visibility (Private or Public) feature.
    - Transparency doesn't work for images upload for some reasons
    - Add more options to edit galleries (a whole dedicated page for example).
    - Quick option to delete a gallery directly from upload-content.php.
    - Within objects, avoid using methods/functions to call for variables, instead use them directly (this's what private is for).
    - Redesign and maybe a modal version of login, and a php with status only for registration and login (success, fail, error etc).
    - Recover Password
    
    Minor priorities:
    - Migrate from MongoDB to SurrealDB (Experimental)

---
+++ public_html/help.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <?php
+    include_once(dirname(__FILE__) . '/common/common-head.php');
+    ?>
+    <link rel="canonical" href="https://tales.anonymousgca.eu/help">
+    <title>Help - Tales</title>
+</head>
+<body class="font-monospace text-light bg-dark">
+
+<?php
+session_start();
+// Check if logged in.
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false) {
+    header("Location: ../login.php");
+    exit();
+}
+?>
+
+<?php
+// Load all necessary includes.
+include_once(dirname(__FILE__) . '/common/utility.php');
+include_once(dirname(__FILE__) . '/../private/objects/User.php');
+include_once(dirname(__FILE__) . '/../private/objects/Content.php');
+include_once(dirname(__FILE__) . '/../private/objects/Followers.php');
+
+// Get user from session
+$user = new User();
+$user->setUsername($_SESSION["username"]);
+$user->loadUser();
+
+?>
+
+<div class="container-fluid">
+    <!-- Navbar -->
+    <div class="row justify-content-between border-bottom pt-2 pb-2">
+        <div class="col-2">
+            <!-- Logo (common/favicon.webp) -->
+            <a href="home.php">
+                <img src="common/favicon.webp" alt="GCA's Baseline" width="40" height="40" data-bs-toggle="tooltip" data-bs-placement="right" title="Homepage">
+            </a>
+        </div>
+        <!-- Search box here -->
+        <div class="col-6 col-md-7 col pe-0 d-flex align-items-center">
+            <form class="w-100" action="search.php" method="GET">
+                <div class="input-group">
+                    <input class="form-control form-control-sm border-0 rounded-3" type="search" placeholder="Search" aria-label="Search" name="search">
+                    <button class="btn btn-sm btn-outline-custom" type="submit"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+        <div class="col-4 col-md-3 ps-0">
+            <div class="row justify-content-end text-end">
+                <div class="col-auto mt-2 pe-0">
+                    <!-- Notification -->
+                    <span class="fa-layers fa-fw" id="bell" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-bell"></i>
+                        <?php
+                        if (count($user->getUnreadNotifications()) > 0) {
+                            echo '<span class="fa-layers-counter rounded-5 p-1 px-2" style="background: rgba(255, 15, 123, 0.54);" id="notification-count">' . count($user->getUnreadNotifications()) . '</span>';
+                        }
+                        ?>
+                    </span>
+                    <!-- Dropdown menu -->
+                    <div class="dropdown-menu dropdown-menu-end p-3 bg-dark-subtle notifications-dropdown"
+                         style="width: 300px; max-height: 80vh; overflow-y: auto;" data-aos="zoom-in">
+                        <h5 class="text-white">Notifications</h5>
+                        <hr class="text-white">
+                        <?php
+                        $notifications = $user->getAllNotifications();
+                        if (empty($notifications)) {
+                            echo '<p class="text-white">You have no notifications.</p>';
+                        } else {
+                            // Button to delete all notification that appears only on hover of the dropdown.
+                            echo '<button class="btn btn-outline-danger w-100 btn-sm" id="delete-notifications" data-bs-toggle="tooltip" data-bs-placement="left" title="Click to delete all notifications">Clear all</button>';
+                            echo '<hr class="text-white notification-divider">';
+                            foreach ($notifications as $notification) {
+                                $title = $notification->getTitle();
+                                $description = $notification->getDescription();
+                                $type = $notification->getNotificationType();
+                                $date = $notification->getNotificationDate();
+                                $viewed = $notification->getViewed();
+                                switch ($type) {
+                                    case 'new_content':
+                                        $color = 'rgba(0, 97, 255, 1)';
+                                        $icon = 'fas fa-bell';
+                                        break;
+                                    case 'new_comment':
+                                        $color = 'rgba(255, 15, 123, 1)';
+                                        $icon = 'fas fa-comment';
+                                        break;
+                                    case 'new_like':
+                                        $color = 'rgba(255, 0, 0, 1)';
+                                        $icon = 'fas fa-heart';
+                                        break;
+                                    case 'new_friend':
+                                        $color = 'rgba(0, 255, 0, 1)';
+                                        $icon = 'fas fa-user-friends';
+                                        break;
+                                    case 'new_follow':
+                                        $color = 'rgba(255, 255, 0, 1)';
+                                        $icon = 'fas fa-user-plus';
+                                        break;
+                                    case 'advertisement':
+                                        $color = 'rgba(255, 255, 255, 1)';
+                                        $icon = 'fas fa-ad';
+                                        break;
+                                    default:
+                                        $color = 'rgba(255, 255, 255, 1)';
+                                        $icon = 'fas fa-bell';
+                                        break;
+                                }
+                                $date = date('d/m/y', strtotime($date));
+                                // If not viewed, add class new-notification
+                                if ($viewed == 0) {
+                                    echo '<div class="d-flex align-items-start mb-2 rounded-3 notification new-notification pt-2 pb-2">';
+                                } else {
+                                    echo '<div class="d-flex align-items-start mb-2 notification rounded-3">';
+                                }
+                                echo '<i class="' . $icon . '" style="color: ' . $color . '; font-size: 24px;"></i>';
+                                echo '<div class="ms-2">';
+                                echo '<h6 class="text-white">' . $title . '</h6>';
+                                echo '<p class="text-white mb-1">' . $description . '</p>';
+                                echo '<small class="text-white">' . $date . '</small>';
+                                echo '</div>';
+                                echo '</div>';
+                                echo '<hr class="text-white notification-divider">';
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <!-- Profile icon that when clicked opens a dropdown menu, aligned to end -->
+                    <div class="dropdown float-end">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fas fa-user"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink"
+                            data-aos="fade-in">
+                            <li><a class="dropdown-item" href="profile.php">Profile</a></li>
+                            <li><a class="dropdown-item" href="settings.php">Settings</a></li>
+                            <li><a class="dropdown-item" href="help.php">Help</a></li>
+                            <li><a class="dropdown-item" id="logout-button" href="actions/logout.php">Logout</a></li>
+                            <!-- Upload button -->
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item text-center text-light border-top border-bottom pt-2 pb-2 rounded-4 bg-gradient"
+                                   id="upload-button" href="upload-content.php">Upload</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Content -->
+    <div class="row mb-3">
+        <div class="col">
+            <!-- Work In progress - Under construction -->
+            <div class="container text-center py-5">
+                <h1 class="display-4">Work In Progress</h1>
+                <p class="lead">This page is under construction and will be available soon.</p>
+                <i class="fas fa-tools fa-5x text-warning"></i>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<?php
+include_once(dirname(__FILE__) . '/common/common-footer.php');
+include_once(dirname(__FILE__) . '/common/common-body.php');
+?>
+<script>
+    $(function (){
+        // Get the bell element
+        var bell = document.getElementById("bell");
+        // Add a click event listener to the bell
+        $('#bell').on("click", function() {
+            // Get the dropdown menu element
+            var dropdown = document.querySelector(".notifications-dropdown");
+            // Set a timeout of 1 second after the dropdown is opened
+            setTimeout(function() {
+                // Check if the dropdown is still open
+                if (dropdown.classList.contains("show")) {
+                    // Get the new-notification elements
+                    var newNotifications = document.querySelectorAll(".new-notification");
+                    // Remove the new-notification class from the newNotifications
+                    for (var i = 0; i < newNotifications.length; i++) {
+                        newNotifications[i].classList.remove("new-notification");
+                    }
+                    // Send a post request to the server to mark all notifications as read
+                    $.ajax({
+                        type: "POST",
+                        url: "actions/notifications.php",
+                        data: {read: true},
+                        success: function() {
+                            // Remove the element notification-count
+                            $('#notification-count').remove();
+                        },
+                        error: function() {
+                            // Show an error message
+                            console.log("Error while marking notifications as read");
+                        }
+                    });
+                }
+            }, 1000);
+        });
+    });
+
+    $(function(){
+        // Handle the deletion of all notifications on click of button #delete-notifications.
+        $('#delete-notifications').on("click", function() {
+            // Send a post request to the server to delete all notifications
+            $.ajax({
+                type: "POST",
+                url: "actions/notifications.php",
+                data: {delete: true},
+                success: function() {
+                    // Remove all the elements with class .notification
+                    $('.notification').remove();
+                    // Remove the element notification-count
+                    $('#notification-count').remove();
+                    // Remove notification-divider
+                    $('.notification-divider').remove();
+                    // Remove button #delete-notifications
+                    $('#delete-notifications').remove();
+                    // And replace it with text: You have no notifications.
+                    $('.notifications-dropdown').append('<p class="text-white">You have no notifications.</p>');
+                },
+                error: function() {
+                    // Show an error message
+                    console.log("Error while deleting notifications");
+                }
+            });
+        });
+    });
+
+    function hideSpinner(image) {
+        image.classList.remove("bg-placeholder");
+        image.style.opacity = "1";
+    }
+</script>
+</body>
+</html>
